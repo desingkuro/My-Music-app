b80215719b9f84a9be55ab5f80aab230e7992d87
desingkuro <andresmauricio745@gmail.com>
2024-09-11 18:09:08 -0500

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import LoginComponent from './pages/authentication/login/login.component';
 
 export const routes: Routes = [
     {
         path:'login',
-        loadChildren: ()=> import('./pages/authentication/login/login.component'),
+        component:LoginComponent,
     },
     { path: 'home', component: AppComponent },
     { path: '**', redirectTo: 'home' }
