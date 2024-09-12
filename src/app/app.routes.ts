d2130b1399017b8da3b42351166b2c408dcbd2df
desingkuro <andresmauricio745@gmail.com>
2024-09-12 12:42:27 -0500

    finalizacion de maquetacion y estilizacion de page login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import LoginComponent from './pages/authentication/login/login.component';
+import { RegisterComponent } from './pages/authentication/register/register.component';
 
 export const routes: Routes = [
     {
         path:'login',
         component:LoginComponent,
+    },
+    {
+        path:'register',
+        component:RegisterComponent,
     },
     { path: 'home', component: AppComponent },
     { path: '**', redirectTo: 'home' }
